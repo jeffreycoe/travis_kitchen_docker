3cd78756c3069c8bd9678dd2738e1fe09a996c6a
Jeffrey Coe <jeffrey.coe@cerner.com>
2019-06-21 16:17:10 -0400

    Updated CI test
    Signed-off-by: Jeffrey Coe <jeffrey.coe@cerner.com>

--- docker.ps1
+++ docker.ps1
@@ -1,8 +1,8 @@
 Write-Host "Configuring Docker service to listen on TCP port 2375..."
-$dockerSvcBinPath = (Get-WmiObject Win32_Service | ?{$_.Name -eq 'docker'} | Select PathName).PathName
-$dockerSvcBinPath = "$dockerSvcBinPath -H tcp://0.0.0.0:2375 -H npipe:////./pipe/docker_engine"
-Write-Host "Docker Service Path: $dockerSvcBinPath"
+$dockerSvcArgs = (Get-WmiObject Win32_Service | ?{$_.Name -eq 'docker'} | Select PathName).PathName
+$dockerSvcArgs = "$dockerSvcArgs -H tcp://0.0.0.0:2375 -H npipe:////./pipe/docker_engine"
+Write-Host "Docker Service Args: $dockerSvcArgs"
 
-Get-WmiObject Win32_Service -Filter "Name='docker'" | Invoke-WmiMethod -Name Change -ArgumentList @($null,$null,$null,$null,$null, $dockerSvcBinPath) | Out-Null
+Get-WmiObject Win32_Service -Filter "Name='docker'" | Invoke-WmiMethod -Name Change -ArgumentList @($null,$null,$null,$null,$null, $dockerSvcArgs) | Out-Null
 
 Restart-Service docker -Force -Verbose
