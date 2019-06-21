3984c5aebbf61c7a91ba7c391e64dccc305a8dbb
Jeffrey Coe <jeffrey.coe@cerner.com>
2019-06-21 15:45:43 -0400

    Updated CI test
    Signed-off-by: Jeffrey Coe <jeffrey.coe@cerner.com>

--- docker.ps1
+++ docker.ps1
@@ -1,7 +1,8 @@
 Write-Host "Configuring Docker service to listen on TCP port 2375..."
-$configFile = "C:\ProgramData\Docker\config\daemon.json"
-$config = (Get-Content $configFile | ConvertFrom-Json)
-$config | Add-Member -Type NoteProperty -Name "hosts" -Value @("tcp://0.0.0.0:2375")
-$config | ConvertTo-Json | Set-Content $configFile
+$dockerSvcBinPath = (Get-WmiObject Win32_Service | ?{$_.Name -eq 'docker'} | Select PathName).PathName
+$dockerSvcBinPath = "$dockerSvcBinPath -H tcp://0.0.0.0:2375 -H npipe:////./pipe/docker_engine"
+Write-Host "Docker Service Path: $dockerSvcBinPath"
+
+Get-WmiObject Win32_Service -Filter "Name='docker'" | Invoke-WmiMethod -Name Change -ArgumentList @($null,$null,$null,$null,$null, $dockerSvcBinPath) | Out-Null
 
 Restart-Service docker -Force -Verbose
