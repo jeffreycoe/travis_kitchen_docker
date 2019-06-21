4aa8ee009d5cf84b06d969aad445229165b5a7e7
Jeffrey Coe <jeffrey.coe@cerner.com>
2019-06-21 16:49:42 -0400

    Updated CI test
    Signed-off-by: Jeffrey Coe <jeffrey.coe@cerner.com>

--- docker.ps1
+++ docker.ps1
@@ -1,3 +1,4 @@
+# This script is used to configure the Docker service for Windows builds in Travis CI
 Write-Host "Configuring Docker service to listen on TCP port 2375..."
 $dockerSvcArgs = (Get-WmiObject Win32_Service | ?{$_.Name -eq 'docker'} | Select PathName).PathName
 $dockerSvcArgs = "$dockerSvcArgs -H tcp://0.0.0.0:2375 -H npipe:////./pipe/docker_engine"
