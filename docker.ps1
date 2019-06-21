af53b75645551de4fec36311315c4b29cd742bdf
Jeffrey Coe <jeffrey.coe@cerner.com>
2019-06-21 14:39:33 -0400

    Updated CI test
    Signed-off-by: Jeffrey Coe <jeffrey.coe@cerner.com>

---
+++ docker.ps1
@@ -0,0 +1,7 @@
+Write-Host "Configuring Docker service to listen on TCP port 2375..."
+$configFile = "C:\ProgramData\Docker\config\daemon.json"
+$config=(Get-Content $configFile | ConvertFrom-Json)
+$config | Add-Member -Type NoteProperty -Name "hosts" -Value @("tcp://0.0.0.0:2375")
+$config | ConvertTo-Json | Set-Content $configFile
+
+Restart-Service docker -Force -Verbose
